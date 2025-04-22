1b41b2ebacada834ff2b1dd3ab5315b54a66d382
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-04-22 11:42:24 -0500

    chore: add environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: "https://newsapi.org/v2/",
+  newsProviderNewsEndpointPath: "/top-headlines",
+  newsProviderSourcesEndpointPath: "/top-headlines/sources",
+  newsProviderApiKey: "de807f23288d4d4e8589afcd7e7df1b4",
+  logoProviderApiBaseUrl: "https://logo.clearbit.com",
+};
